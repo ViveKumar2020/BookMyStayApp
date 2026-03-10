a8461284ee38ad52851c1b06fb91d9e39ad67948
ViveKumar2020 <vp6068@srmist.edu.in>
2026-03-10 09:30:43 +0530

    UC2: Basic Room Types & Static Availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,9 +1,67 @@
-public class BookMyStayApp{
+
+public class BookMyStayApp {
+
+    static abstract class Room {
+        int beds;
+        double price;
+        int size;
+
+        Room(int beds, double price, int size) {
+            this.beds = beds;
+            this.price = price;
+            this.size = size;
+        }
+
+        void displayDetails() {
+            System.out.println("Beds: " + beds);
+            System.out.println("Price: Rs " + price);
+            System.out.println("Size: " + size + " ft");
+        }
+    }
+
+    // Single Room
+    static class SingleRoom extends Room {
+        SingleRoom() {
+            super(1, 2000,300);
+        }
+    }
+
+    // Double Room
+    static class DoubleRoom extends Room {
+        DoubleRoom() {
+            super(2, 3000, 500);
+        }
+    }
+
+    // Suite Room
+    static class SuiteRoom extends Room {
+        SuiteRoom() {
+            super(3, 5000, 900);
+        }
+    }
 
     public static void main(String[] args) {
 
-        System.out.println(" Welcome to the Book My Stay App ");
-        System.out.println(" Application Version: v1.0 ");
+        Room single = new SingleRoom();
+        Room doubleroom = new DoubleRoom();
+        Room suite = new SuiteRoom();
 
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        System.out.println("Welcome to BookMyStay\n");
+
+        System.out.println("Single Room:");
+        single.displayDetails();
+        System.out.println("Available: " + singleAvailable);
+
+        System.out.println("\nDouble Room:");
+        doubleroom.displayDetails();
+        System.out.println("Available: " + doubleAvailable);
+
+        System.out.println("\nSuite Room:");
+        suite.displayDetails();
+        System.out.println("Available: " + suiteAvailable);
     }
 }
