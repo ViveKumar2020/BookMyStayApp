f9082f5063e43c6a7b82fe5f4e66bb0d09a5705b
ViveKumar2020 <vp6068@srmist.edu.in>
2026-03-10 09:05:37 +0530

    UC1: Application Entry & Welcome Message

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,3 +1,9 @@
-public class BookMyStayApp {
+public class BookMyStayApp{
 
+    public static void main(String[] args) {
+
+        System.out.println(" Welcome to the Book My Stay App ");
+        System.out.println(" Application Version: v1.0 ");
+
+    }
 }
