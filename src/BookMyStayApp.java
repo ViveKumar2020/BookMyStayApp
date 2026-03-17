056a29734353442e62271954515823aec464c7c7
ViveKumar2020 <vp6068@srmist.edu.in>
2026-03-17 09:11:03 +0530

    Update BookMyStayApp.java

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,13 +1,11 @@
 import java.util.*;
 
-// Custom Exception for invalid bookings
 class InvalidBookingException extends Exception {
     public InvalidBookingException(String message) {
         super(message);
     }
 }
 
-// Reservation class
 class Reservation {
     private String reservationId;
     private String guestName;
@@ -46,19 +44,17 @@ class Reservation {
     }
 }
 
-// Booking system with validation
 class BookingSystem {
 
     private Map<String, Integer> roomInventory = new HashMap<>();
 
     public BookingSystem() {
-        // Initial inventory
+        
         roomInventory.put("Standard", 2);
         roomInventory.put("Deluxe", 2);
         roomInventory.put("Suite", 1);
     }
 
-    // Validate booking request
     public void validateBooking(String roomType, int nights) throws InvalidBookingException {
 
         if (!roomInventory.containsKey(roomType)) {
@@ -74,20 +70,16 @@ class BookingSystem {
         }
     }
 
-    // Confirm booking
     public Reservation confirmBooking(String reservationId, String guestName, String roomType, int nights)
             throws InvalidBookingException {
 
-        // Fail-fast validation
         validateBooking(roomType, nights);
 
-        // Update inventory safely
         roomInventory.put(roomType, roomInventory.get(roomType) - 1);
 
         return new Reservation(reservationId, guestName, roomType, nights);
     }
 
-    // Display inventory
     public void displayInventory() {
         System.out.println("\nCurrent Room Inventory:");
         for (String room : roomInventory.keySet()) {
@@ -96,7 +88,6 @@ class BookingSystem {
     }
 }
 
-// Main Application
 public class BookMyStayApp {
 
     public static void main(String[] args) {
@@ -107,18 +98,15 @@ public class BookMyStayApp {
 
         try {
 
-            // Valid booking
             Reservation r1 = system.confirmBooking("RES201", "Amit", "Deluxe", 2);
             System.out.println("\nBooking Successful:");
             System.out.println(r1);
 
-            // Invalid room type
             Reservation r2 = system.confirmBooking("RES202", "Riya", "Premium", 1);
             System.out.println(r2);
 
         } catch (InvalidBookingException e) {
 
-            // Graceful error handling
             System.out.println("\nBooking Failed: " + e.getMessage());
         }
 
